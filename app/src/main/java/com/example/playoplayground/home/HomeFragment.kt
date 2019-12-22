062d1df01422607698a56c313849865d4108996c
rohiththammaiah <rohit@iciindia.in>
2019-12-22 18:02:18 +0530

    created a controller for epoxy

--- app/src/main/java/com/example/playoplayground/home/HomeFragment.kt
+++ app/src/main/java/com/example/playoplayground/home/HomeFragment.kt
@@ -4,13 +4,22 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.airbnb.epoxy.EpoxyController
 import com.airbnb.mvrx.BaseMvRxFragment
 import com.airbnb.mvrx.MvRxState
 import com.airbnb.mvrx.fragmentViewModel
 import com.airbnb.mvrx.withState
 import com.example.playoplayground.R
+import kotlinx.android.synthetic.main.home_fragment.*
 
 data class HomeState(val title: String = "Home") : MvRxState
+
+class HomeEpoxyController(): EpoxyController() {
+    override fun buildModels() {
+
+    }
+
+}
 
 class HomeFragment : BaseMvRxFragment() {
 
@@ -25,6 +34,7 @@ class HomeFragment : BaseMvRxFragment() {
 
     override fun invalidate(): Unit = withState(viewModel) { state ->
         requireActivity().title = state.title
+        home_epoxy_list.setController(HomeEpoxyController())
     }
 
 }
